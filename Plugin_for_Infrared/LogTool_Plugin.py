53e156902e08791f0957b28a5186acfd6ca2c882
Arkady Shtempler <ashtempl@redhat.com>
2019-05-23 15:34:13 +0300

    Goodbye word!!!

--- Plugin_for_Infrared/LogTool_Plugin.py
+++ Plugin_for_Infrared/LogTool_Plugin.py
@@ -135,6 +135,6 @@ class LogTool(unittest.TestCase):
         if len(failed_nodes)!=0:
             append_to_file(report_file_name,'Failed - Errors have been detected on: '+str(failed_nodes.keys())+
                         '\nDetected Unique ERRORs are:'+'\n'*5+detected_unique_errors+
-                          '\n*** Check LogTool result files in: "'+os.path.abspath(result_dir)+'" for more details')
+                          '\n*** For more details, check LogTool result files on your setup: '+os.path.abspath(result_dir))
 
 
