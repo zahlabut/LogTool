53e90a2437c18b03c9f5bd1472472e6ac2a1b47d
Arkady Shtempler <ashtempl@redhat.com>
2019-07-04 13:44:22 +0300

    Goodbye word!!!

--- LogTool_Python3/Common.py
+++ LogTool_Python3/Common.py
@@ -187,6 +187,7 @@ def choose_option_from_list(list_object, msg):
         print('*** No such option!!!***', e)
         return[False, str(e)]
 
+
 def exit(string):
     print_in_color(string,'red')
     sys.exit(1)
