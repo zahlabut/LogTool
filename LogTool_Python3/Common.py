d9a1c13e2d9de1aea3ca9127125f50bd1e0931ca
Arkady Shtempler <ashtempl@redhat.com>
2019-07-04 11:42:53 +0300

    Goodbye word!!!

--- LogTool_Python3/Common.py
+++ LogTool_Python3/Common.py
@@ -146,7 +146,7 @@ def exec_command_line_command(command):
         json_output = None
 
         #try:
-        json_output = json.loads(result.lower())
+        json_output = json.loads(str(result.lower()))
         #except:
         #    pass
         return {'ReturnCode': 0, 'CommandOutput': result, 'JsonOutput': json_output}
