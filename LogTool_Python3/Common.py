72c55652dd1a344a62d180f637202521e0c04ba2
Arkady Shtempler <ashtempl@redhat.com>
2019-07-04 11:44:26 +0300

    Goodbye word!!!

--- LogTool_Python3/Common.py
+++ LogTool_Python3/Common.py
@@ -143,10 +143,12 @@ def exec_command_line_command(command):
         command_as_list = [item.replace(' ', '') for item in command_as_list if item != '']
         result = subprocess.check_output(command, shell=True)
         result=str(result)
+
+        print(result)
         json_output = None
 
         #try:
-        json_output = json.loads(str(result.lower()))
+        json_output = json.loads(result.lower())
         #except:
         #    pass
         return {'ReturnCode': 0, 'CommandOutput': result, 'JsonOutput': json_output}
