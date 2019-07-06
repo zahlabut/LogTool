4d7923b6ec08f0a861f5eb47e9b204663c46f05d
Arkady Shtempler <ashtempl@redhat.com>
2019-07-06 14:04:05 +0300

    Goodbye word!!!

--- LogTool_Python3/PyTool.py
+++ LogTool_Python3/PyTool.py
@@ -27,8 +27,8 @@ mode_execution_status={}
 
 empty_file_content('Runtime.log')
 empty_file_content('Error.log')
-sys.stdout=MyOutput('Runtime.log')
-sys.stderr=MyOutput('Error.log')
+# sys.stdout=MyOutput('Runtime.log')
+# sys.stderr=MyOutput('Error.log')
 
 # On interrupt "ctrl+c" executed script will be killed
 executed_script_on_overcloud = []
@@ -293,13 +293,7 @@ if mode[1]=='"Grep" some string for all Overcloud logs':
     nodes = exec_command_line_command('source ' + source_rc_file_path + 'stackrc;openstack server list -f json')['JsonOutput']
     nodes = [{'Name': item['name'], 'ip': item['networks'].split('=')[-1]} for item in nodes]
     print_in_color("1) You can use special characters in your string\n2) Ignore case sensitive flag is used by default",'yellow')
-
-
     string_to_grep = "'"+input("Please enter your 'grep' string: ")+"'"
-
-
-
-
     start_time = time.time()
     result_dir='All_Greped_Strings'
     if result_dir in os.listdir('.'):
