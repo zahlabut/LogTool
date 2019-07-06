0ffbd125655993de35fa077d829f21169df2d3b6
Arkady Shtempler <ashtempl@redhat.com>
2019-07-06 14:34:58 +0300

    Goodbye word!!!

--- LogTool_Python3/Download_Logs_By_Timestamp.py
+++ LogTool_Python3/Download_Logs_By_Timestamp.py
@@ -11,7 +11,7 @@ def exec_command_line_command(command):
     try:
         command_as_list = command.split(' ')
         command_as_list = [item.replace(' ', '') for item in command_as_list if item != '']
-        result = subprocess.check_output(command, shell=True)
+        result = subprocess.check_output(command, shell=True, encoding='UTF-8')
         json_output = None
         try:
             json_output = json.loads(result.lower())
