5b1684695c0ce1bb8d035167766353b38a2d5c81
Arkady Shtempler <ashtempl@redhat.com>
2019-07-06 14:27:29 +0300

    Goodbye word!!!

---
+++ LogTool_Python3/UserScripts/Overcloud_Node_Simple_Commands.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+import os
+print(os.system('date'))
+print(os.system('pwd'))
+print(os.system('ls -ltrh'))
---
+++ LogTool_Python3/UserScripts/Simple_Bash_Script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "---Bash script start!---"
+echo $(date)
+echo $(hostname)
+echo "---Bash script stop!---"
