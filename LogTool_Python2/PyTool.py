d3d5d455680f9fcf0c775bf66ebe6e3091c383f9
Arkady Shtempler <ashtempl@redhat.com>
2019-09-15 11:03:02 +0300

    G

--- LogTool_Python2/PyTool.py
+++ LogTool_Python2/PyTool.py
@@ -108,9 +108,9 @@ try:
                     for w in magic_words:
                         if w in item:
                             magic_dic_result[w].append(item)
-        append_to_file(result_file,'\n'*10+'#'*50+'Unique statistics for these magic keys:'+str(magic_words)+'#'*50+'\n\n\n')
+        append_to_file(result_file,'\n'*10+'#'*50+' Unique statistics for these magic keys:'+str(magic_words)+' '+'#'*50+'\n\n\n')
         for key in magic_dic_result:
-            append_to_file(result_file,'\n' + '_' * 40 + key + '_' * 40+'\n')
+            append_to_file(result_file,'\n\n\n' + '_' * 40 + key + '_' * 40+'\n')
             if key in ['fatal','FAILED','ERROR']:
                 fuzzy=1
             else:
