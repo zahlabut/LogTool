2bf70abe5d24ffcda323cb2d14a53c158926a996
Arkady Shtempler <ashtempl@redhat.com>
2019-09-16 10:31:18 +0300

    Add new mode

--- LogTool_Python2/PyTool.py
+++ LogTool_Python2/PyTool.py
@@ -122,9 +122,9 @@ try:
                 for w in magic_words:
                     if w in item:
                         if w == 'TASK':
-                            magic_dic_result[w].append(item[item.find(w):].replace('*',''))
+                            magic_dic_result[w].append('###'+item[item.find(w):].replace('*','')+'###')
                         else:
-                            magic_dic_result[w].append(item)
+                            magic_dic_result[w].append('###'+item+'###')
         append_to_file(result_file,'\n'*10+'#'*50+' Unique statistics for these magic keys:'+str(magic_words)+' '+'#'*50+'\n\n\n')
         for key in magic_dic_result:
             append_to_file(result_file,'\n\n\n' + '_' * 40 + key + '_' * 40+'\n')
