5eb9cab47c3681b402a529faaa264307f0ed741b
Arkady Shtempler <ashtempl@redhat.com>
2019-09-16 10:18:26 +0300

    Add new mode

--- LogTool_Python2/PyTool.py
+++ LogTool_Python2/PyTool.py
@@ -121,7 +121,7 @@ try:
                 append_to_file(result_file,item)
                 for w in magic_words:
                     if w in item:
-                        magic_dic_result[w].append(item)
+                        magic_dic_result[w].append(item[item.find(w):].replace('*',''))
         append_to_file(result_file,'\n'*10+'#'*50+' Unique statistics for these magic keys:'+str(magic_words)+' '+'#'*50+'\n\n\n')
         for key in magic_dic_result:
             append_to_file(result_file,'\n\n\n' + '_' * 40 + key + '_' * 40+'\n')
