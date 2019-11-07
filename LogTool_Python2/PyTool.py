29b5946bc0723bd28e49cd5416c43ed6cd1dfdd8
Arkady Shtempler <ashtempl@redhat.com>
2019-11-07 13:02:55 +0200

    109

--- LogTool_Python2/PyTool.py
+++ LogTool_Python2/PyTool.py
@@ -113,7 +113,7 @@ try:
                         append_to_file(result_file, line+'\n')
                     else:
                         if w_index+1000<len(line):
-                            append_to_file(result_file, '...Line is too long ...' + line[w_index:w_index+1000] + '...'+'\n')
+                            append_to_file(result_file, '...Line is too long ...' + line[w_index:w_index+1000] + '...Line is too long ...'+'\n')
                         else:
                             append_to_file(result_file, '...Line is too long ...' + line[w_index:] + '\n')
                     break
