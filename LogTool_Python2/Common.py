a638c360ded25a3faf0a8b3462e4bcd4ae7122cc
Arkady Shtempler <ashtempl@redhat.com>
2019-12-16 11:50:20 +0200

    Adding pip requirements.txt file

--- LogTool_Python2/Common.py
+++ LogTool_Python2/Common.py
@@ -152,7 +152,6 @@ class SSH():
 
 def exec_command_line_command(command):
     try:
-        print_in_color('--> '+command, 'blue')
         command_as_list = command.split(' ')
         command_as_list = [item.replace(' ', '') for item in command_as_list if item != '']
         result = subprocess.check_output(command, stdin=True, stderr=subprocess.STDOUT, shell=True)
