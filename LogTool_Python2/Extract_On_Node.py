cffcffaadc62a83807a79b13937d8151d99a89d2
Arkady Shtempler <ashtempl@redhat.com>
2020-04-22 10:26:03 +0300

    Julia's bug fix.

--- LogTool_Python2/Extract_On_Node.py
+++ LogTool_Python2/Extract_On_Node.py
@@ -213,10 +213,10 @@ def analyze_log(log, string, time_grep, file_to_save,last_line_date):
         basic_strings=['WARNING',string]
         strings=basic_strings
     if 'ERROR' in string:
-        basic_strings=[' ERROR',' CRITICAL',' FATAL',' TRACE','|ERR|',' FAILED', ' STDERR',' traceback']
+        basic_strings=[' ERROR',' CRITICAL',' FATAL',' TRACE','|ERR|']#,' FAILED', ' STDERR',' traceback']
         strings=basic_strings+python_exceptions
     for item in strings:
-        command+="grep -B2 -A7 -i '"+item+"' " + log + " >> "+grep_file+";echo -e '--' >> "+grep_file+';'
+        command+="grep -B2 -A7 '"+item+"' " + log + " >> "+grep_file+";echo -e '--' >> "+grep_file+';'
     if log.endswith('.gz'):
         command.replace('grep','zgrep')
     exec_command_line_command(command)
