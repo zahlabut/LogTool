4c51da9f994377e0c7337bac623a8199a524c208
Arkady Shtempler <ashtempl@redhat.com>
2020-07-02 20:28:29 +0300

    IR plugin update

--- Plugin_For_Infrared_Python3/LogTool_Plugin.py
+++ Plugin_For_Infrared_Python3/LogTool_Plugin.py
@@ -164,7 +164,7 @@ class LogTool(unittest.TestCase):
             if 'Total_Number_Of_Errors:0' not in workers_output[key]:
                 report_data+='Errors have been detected on:'+key+'\n'+ workers_output[key]
         if len(report_data)!=0:
-            append_to_file(report_file_name,'Failed - Errors have been detected on: '+str(list(failed_nodes.keys()))+
+            append_to_file(report_file_name,'Failed - Errors have been detected on: '+str(list(workers_output.keys()))+
                           '\n*** For more details, check LogTool result files on your setup: '+os.path.abspath(result_dir))
 
         # failed_nodes={}
