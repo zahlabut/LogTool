9c464803e7b705ff7bb9bac3294f6a42b7b59520
Arkady Shtempler <ashtempl@redhat.com>
2020-07-03 08:33:59 +0300

    IR plugin update

--- Plugin_For_Infrared_Python3/LogTool_Plugin.py
+++ Plugin_For_Infrared_Python3/LogTool_Plugin.py
@@ -127,11 +127,11 @@ class LogTool(unittest.TestCase):
     def test_2_Export_Undercloud_Errors(self):
         print('\ntest_2_Export_Undercloud_Errors')
         mode_start_time = time.time()
-        for dir in undercloud_logs_dir:
-            result_file = 'Undercloud'+dir.replace('/','_')+'.log'
-            command="sudo python3 Extract_On_Node.py '" + str(user_start_time) + "' " + "'" +dir+ "'" + " '" + grep_string + "'" + ' ' + result_file
-            com_result=exec_command_line_command(command)
-            shutil.move(result_file+'.gz', os.path.join(os.path.abspath(result_dir),result_file+'.gz'))
+
+        result_file = 'Undercloud.log'
+        command="sudo python3 Extract_On_Node.py '" + str(user_start_time) + "' " + "'" +dir+ "'" + " '" + grep_string + "'" + ' ' + result_file
+        com_result=exec_command_line_command(command)
+        shutil.move(result_file+'.gz', os.path.join(os.path.abspath(result_dir),result_file+'.gz'))
         end_time=time.time()
         if com_result['ReturnCode']==0:
             spec_print(['Completed!!!','Result Directory: '+result_dir,'Execution Time: '+str(end_time-mode_start_time)+'[sec]'],'green')
