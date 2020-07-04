ea599659216069e77b0d372df76c5e692a2ed6d0
Arkady Shtempler <ashtempl@redhat.com>
2020-07-04 12:01:29 +0300

    IR plugin update

--- JenkinsStage/Bash_Script_For_Freestyle_Job.sh
+++ JenkinsStage/Bash_Script_For_Freestyle_Job.sh
@@ -1,3 +1,19 @@
+#rm -rf LogTool
+#rm -rf infrared
+#wget http://google.com
+#yum install -y wget
+#git clone https://github.com/redhat-openstack/infrared.git
+#virtualenv .venv && source .venv/bin/activate
+#cd infrared;pip install --upgrade pip;pip install --upgrade setuptools;pip install paramiko;
+#pip install beautifulsoup4;pip install request;pip install .
+#ir plugin remove logtool
+#ir plugin add https://github.com/zahlabut/LogTool.git --src-path Plugin_For_Infrared_Python2
+#ir workspace delete workspace > /dev/null
+#ir workspace import $workspace_url
+#ir logtool
+
+
+
 
 
 
