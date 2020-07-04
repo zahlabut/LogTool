1c81e1e2a19b0666f49a13553d8ea3b6d9d911d7
Arkady Shtempler <ashtempl@redhat.com>
2020-07-04 11:57:03 +0300

    IR plugin update

--- JenkinsStage/Bash_Script_For_Freestyle_Job.sh
+++ JenkinsStage/Bash_Script_For_Freestyle_Job.sh
@@ -9,4 +9,4 @@ pip install paramiko
 git clone https://github.com/zahlabut/LogTool.git
 echo "start_time='"$user_tart_time"'" >> LogTool/JenkinsStage/Params.py
 echo "artifact_url='"$artifact_url"'" >> LogTool/JenkinsStage/Params.py
-cd LogTool/Plugin_For_Infrared_Python2; python -m unittest JenkinsStage.LogTool.test_1_download_jenkins_job
+cd LogTool/JenkinsStage; python -m unittest JenkinsStage.LogToolStage.test_1_download_jenkins_job
