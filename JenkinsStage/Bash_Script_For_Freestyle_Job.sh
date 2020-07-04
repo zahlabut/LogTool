939b38cfe03b38131d35c0eaffbeb04971dbdc4f
Arkady Shtempler <ashtempl@redhat.com>
2020-07-04 12:03:28 +0300

    IR plugin update

--- JenkinsStage/Bash_Script_For_Freestyle_Job.sh
+++ JenkinsStage/Bash_Script_For_Freestyle_Job.sh
@@ -27,4 +27,4 @@ pip install paramiko
 git clone https://github.com/zahlabut/LogTool.git
 echo "start_time='"$user_tart_time"'" >> LogTool/JenkinsStage/Params.py
 echo "artifact_url='"$artifact_url"'" >> LogTool/JenkinsStage/Params.py
-cd LogTool/JenkinsStage; python -m unittest LogToolStage.test_1_download_jenkins_job
+cd LogTool/JenkinsStage; python -m unittest LogToolStage.LogTool.test_1_download_jenkins_job
