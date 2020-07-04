38cf46ad09bce2f7ad9b596b75be0f911f456805
Arkady Shtempler <ashtempl@redhat.com>
2020-07-04 13:09:18 +0300

    IR plugin update

--- JenkinsStage/LogToolStage.py
+++ JenkinsStage/LogToolStage.py
@@ -132,7 +132,7 @@ class LogTool(unittest.TestCase):
         end_time = time.time()
         if 'SUCCESS!!!' in com_result['CommandOutput']:
 
-            spec_print(com_result['CommandOutput'].splitlines()[-10:],'blue')
+            spec_print(com_result['CommandOutput'].splitlines()[-3:],'blue')
 
             spec_print(['Completed!!!', 'You can find the result file + downloaded logs in:',
                         "All the results are in Job's Build Artifacts Directory",
