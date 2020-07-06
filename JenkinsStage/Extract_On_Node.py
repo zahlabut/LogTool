27b60173a7cb8ee25929d067cde356158aa94acf
Arkady Shtempler <ashtempl@redhat.com>
2020-07-06 22:12:16 +0300

    Sort exported unique Error blocks by timestamps

--- JenkinsStage/Extract_On_Node.py
+++ JenkinsStage/Extract_On_Node.py
@@ -668,6 +668,7 @@ if __name__ == "__main__":
         else:
             #append_to_file(html_page,'<a href="'+str(item)+'"></a>\n')
             print_in_color(item.items(),'red')
+            append_to_file(html_page, '<a href="' + item.items()[0] + '"></a>\n')
 
     #{'/home/stack/Jenkins_Job_Files/06-ir-tripleo-overcloud-introspect.log': 23}
 
