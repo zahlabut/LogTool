0c56f6501c118a8abc9f15ec46018671b494363f
Arkady Shtempler <ashtempl@redhat.com>
2020-07-06 21:08:22 +0300

    Sort exported unique Error blocks by timestamps

--- JenkinsStage/Extract_On_Node.py
+++ JenkinsStage/Extract_On_Node.py
@@ -655,9 +655,9 @@ if __name__ == "__main__":
                                '\n\n\n'+'#'*20+' Statistics - Number of Errors/Warnings per Standard OSP log since: '+time_grep+' '+'#'*20+'\n')
     #Create HTTML index and Directory
     html_directory='ExtractedBlocks'
-    #if os.path.exists(html_directory)==True:
-    #    os.remove(html_directory)
-    os.makedirs(html_directory,False)
+    if os.path.exists(html_directory)==True:
+        os.remove(html_directory)
+    os.makedirs(html_directory)
     html_page='index.html'
     empty_file_content(html_page)
     append_to_file(html_page,'<!DOCTYPE html>\n'+'<html>\n'+'<head>\n'+'<title>Header Tag</title>\n'+'</head>\n'+'<body>\n')
