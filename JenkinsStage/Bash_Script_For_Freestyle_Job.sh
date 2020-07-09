8b91242a75bd925339bdcd6ef7848dc3c73c1863
Arkady Shtempler <ashtempl@redhat.com>
2020-07-09 18:00:44 +0300

    Check user time

--- JenkinsStage/Bash_Script_For_Freestyle_Job.sh
+++ JenkinsStage/Bash_Script_For_Freestyle_Job.sh
@@ -1,33 +1,8 @@
-#rm -rf LogTool
-#rm -rf infrared
-#wget http://google.com
-#yum install -y wget
-#git clone https://github.com/redhat-openstack/infrared.git
-#virtualenv .venv && source .venv/bin/activate
-#cd infrared;pip install --upgrade pip;pip install --upgrade setuptools;pip install paramiko;
-#pip install beautifulsoup4;pip install request;pip install .
-#ir plugin remove logtool
-#ir plugin add https://github.com/zahlabut/LogTool.git --src-path Plugin_For_Infrared_Python2
-#ir workspace delete workspace > /dev/null
-#ir workspace import $workspace_url
-#ir logtool
-
-
-
-
-
-
-# HTTP Download and Analyze log #
-df -h
-hostname
+# Download CI artifact files and Analyze logs #
 virtualenv .venv && source .venv/bin/activate
 pip install beautifulsoup
-pip install request
-pip install paramiko
+pip install requests
 git clone https://github.com/zahlabut/LogTool.git
-echo "start_time='"$user_tart_time"'" >> LogTool/JenkinsStage/Params.py
+echo "user_start_time='"$user_start_time"'" >> LogTool/JenkinsStage/Params.py
 echo "artifact_url='"$artifact_url"'" >> LogTool/JenkinsStage/Params.py
 cd LogTool/JenkinsStage; python -m unittest LogToolStage.LogTool.test_1_download_jenkins_job
-
-
-#Save artifacts with:LogTool/JenkinsStage/Jenkins_Job_ERROR/**/*.*
--- JenkinsStage/Common.py
+++ JenkinsStage/Common.py
@@ -14,7 +14,6 @@
 
 
 import os
-import paramiko
 import time
 import subprocess
 import json
@@ -61,105 +60,6 @@ def print_in_color(string,color_or_format=None):
     else:
         print(string)
 
-class MyOutput():
-    def __init__(self, logfile):
-        self.stdout = sys.stdout
-        self.log = open(logfile, 'w')
-  
-    def write(self, text):
-        self.stdout.write(text)
-        self.log.write(text)
-        #self.log.flush()
-  
-    def close(self):
-        self.stdout.close()
-        self.log.close()
-
-
-def check_ping(ip):
-    try:
-        if subprocess.check_output(["ping", "-c", "1", ip]):
-            return True
-    except Exception as e:
-        print('Ping to '+ip+' failed with '+str(e))
-        return False
-
-class SSH():
-    def __init__(self, host, user, password='', key_path=''):
-        self.host=host
-        self.user=user
-        self.password=password
-        self.key_path=key_path
-
-    def ssh_connect_password(self):
-        try:
-            self.client = paramiko.SSHClient()
-            self.client.load_system_host_keys()
-            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.client.connect(self.host, username=self.user, password=self.password)
-            return {'Status':True}
-        except Exception as e:
-            print_in_color(str(e),'red')
-            return {'Status':False,'Exception':e}
-
-    def ssh_connect_key(self):
-        try:
-            self.client = paramiko.SSHClient()
-            self.client.load_system_host_keys()
-            self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            self.client.connect(self.host, username=self.user, key_filename=self.key_path)
-            return {'Status':True}
-        except Exception as e:
-            print_in_color(str(e), 'red')
-            return {'Status':False,'Exception':e}
-
-    def ssh_command(self, command):
-        stdin,stdout,stderr=self.client.exec_command(command)
-        #stdin.close()
-        self.output=''
-        self.stderr=''
-        for line in stdout.read().decode().splitlines():
-            self.output+=line+'\n'
-        for line in stderr.read().decode().splitlines():
-            self.stderr+=line+'\n'
-        result= {'Stdout':self.output, 'Stderr':self.stderr}
-        if len(result['Stderr'])!=0 and 'warning' in str(result['Stderr']).lower():
-            print_in_color(result['Stderr'],'yellow')
-        else:
-            print_in_color(result['Stderr'], 'red')
-        return result
-
-    def ssh_command_only(self, command):
-        self.stdin,self.stdout,self.stderr=self.client.exec_command(command)
-        return {'Stdout':self.stdout.read(),'Stderr':self.stderr.read().decode()}
-
-    def scp_upload(self, src_abs_path, dst_abs_path):
-        try:
-            file_size=os.path.getsize(src_abs_path)
-            ftp = self.client.open_sftp()
-            t1=time.time()
-            ftp.put(src_abs_path,dst_abs_path)
-            t2=time.time()
-            return {'Status':True,'AverageBW':file_size/(t2-t1),'ExecutionTime':t2-t1}
-        except  Exception as e:
-            print_in_color(str(e), 'red')
-            return {'Status':False,'Exception':e}
-
-    def scp_download(self,remote_abs_path,local_abs_path):
-        try:
-            ftp=self.client.open_sftp()
-            t1 = time.time()
-            ftp.get(remote_abs_path, local_abs_path)
-            t2 = time.time()
-            file_size=os.path.getsize(local_abs_path)
-            return {'Status': True,'AverageBW':file_size/(t2-t1),'ExecutionTime':t2-t1}
-        except  Exception as e:
-            print_in_color(str(e), 'red')
-            return {'Status': False, 'Exception': e}
-
-    def ssh_close(self):
-        self.client.close()
-
 def exec_command_line_command(command):
     try:
         command_as_list = command.split(' ')
@@ -188,30 +88,6 @@ def spec_print(string_list,color=None):
         print_in_color("### "+item.strip()+" "*(max_len-len("### "+item.strip())-4)+" ###",color)
     print_in_color("#"*max_len+'\n',color)
 
-def choose_option_from_list(list_object, msg):
-    print('')
-    try:
-        if (len(list_object)==0):
-            print("Nothing to choose :( ")
-            print("Execution will stop!")
-            time.sleep(5)
-            exit("Connot continue execution!!!")
-            sys.exit(1)
-        print(msg)
-        counter=1
-        for item in list_object:
-            print(str(counter)+') - '+item)
-            counter=counter+1
-        choosed_option=input("Choose your option:")
-        while (int(choosed_option)<0 or int(choosed_option)> len(list_object)):
-            print("No such option - ", choosed_option)
-            choosed_option=input("Choose your option:")
-        print_in_color("Option is: '"+list_object[int(choosed_option)-1]+"'"+'\n','bold')
-        return [True,list_object[int(choosed_option)-1]]
-    except Exception as e:
-        print('*** No such option!!!***', e)
-        return[False, str(e)]
-
 def exit(string):
     print_in_color(string,'red')
     sys.exit(1)
@@ -231,12 +107,7 @@ def check_time(time_string):
     except:
         return False
 
-def download_jenkins_job_logs(node_names_list,url):
-    response = urllib.request.urlopen(url)
-    html= response.read(url)
-
 def append_to_file(log_file, msg):
     log_file = open(log_file, 'a')
     log_file.write(msg)
     log_file.close()
-
