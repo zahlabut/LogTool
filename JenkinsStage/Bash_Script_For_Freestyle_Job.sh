8dd617b682e3b9245a114654240fbe1dfa3b4081
Arkady Shtempler <ashtempl@redhat.com>
2020-07-15 13:11:58 +0300

    Adding advanced options

--- JenkinsStage/Bash_Script_For_Freestyle_Job.sh
+++ JenkinsStage/Bash_Script_For_Freestyle_Job.sh
@@ -5,4 +5,8 @@ pip install requests
 git clone https://github.com/zahlabut/LogTool.git
 echo "user_start_time='"$user_start_time"'" >> LogTool/JenkinsStage/Params.py
 echo "artifact_url='"$artifact_url"'" >> LogTool/JenkinsStage/Params.py
+echo "analyze_overcloud_logs='"$analyze_overcloud_logs"'" >> LogTool/JenkinsStage/Params.py
+echo "overcloud_log_dirs='"$overcloud_log_dirs"'" >> LogTool/JenkinsStage/Params.py
+echo "analyze_undercloud_logs='"$analyze_undercloud_logs"'" >> LogTool/JenkinsStage/Params.py
+echo "undercloud_log_dirs='"$undercloud_log_dirs"'" >> LogTool/JenkinsStage/Params.py
 cd LogTool/JenkinsStage; python -m unittest LogToolStage.LogTool.test_1_download_jenkins_job
