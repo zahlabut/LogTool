09727863fe2a97775b24a1ab808a6205cfd9b14f
Arkady Shtempler <ashtempl@redhat.com>
2020-09-23 18:42:28 +0300

    Add ignore string

--- LogTool_Python3/Extract_On_Node.py
+++ LogTool_Python3/Extract_On_Node.py
@@ -50,7 +50,8 @@ ignore_strings=['completed with no errors','program: Errors behavior:',
                     'No error reported.','--exit-command-arg error','Use errors="ignore" instead of skip.',
                     'Errors:None','errors, 0','errlog_type error ','errorlevel = ','ERROR %(name)s','Total errors: 0',
                 '0 errors,','python-traceback2-','"Error": ""','perl-Errno-','libgpg-error-','libcom_err-',
-                '= CRITICAL ','"Error": "",','stderr F','fatal_exception_format_errors','failed=0   ','--log-level error']
+                '= CRITICAL ','"Error": "",','stderr F','fatal_exception_format_errors','failed=0   ','--log-level error',
+                '--exit - command - arg error']
 
 logs_to_ignore=['/var/lib/containers/storage/overlay', '/var/log/anaconda'] #These logs won't be analysed
 
